5e4a883a70544a57fe06b8198ee46927ae60008a
AlinaShvetsova16 <alinashvetsova16@gmail.com>
2026-05-31 17:37:59 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,10 @@ int main() {
   Train train;
   int count = 60; // кол-во вагонов
 
-  while (count--)
+  while (count--) {
     train.addCar(false);
-
+  }
+  
   std::cout << train.getLength() << std::endl;
   std::cout << train.getOpCount() << std::endl;
   return 0;
